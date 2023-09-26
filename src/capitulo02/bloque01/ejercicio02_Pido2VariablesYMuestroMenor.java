69068461db77e4e489bbba224b610372894c9708
arroyosergi3 <sergioaj815@gmail.com>
2023-09-26 09:17:34 +0200

    feat(chapter2):exercises 1&2 added

---
+++ src/capitulo02/bloque01/ejercicio02_Pido2VariablesYMuestroMenor.java
@@ -0,0 +1,25 @@
+package capitulo02.bloque01;
+
+import java.util.Scanner;
+
+public class ejercicio02_Pido2VariablesYMuestroMenor {
+
+	public static void main(String[] args) {
+
+		double num1, num2;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Introduce un número: ");
+		num1 = sc.nextDouble();
+		
+		System.out.println("Introduce otro número: ");
+		num2 = sc.nextDouble();
+		
+		if (num1 > num2) {
+			System.out.println("El número menor es: " + (int)num2);
+		}
+		else {
+			System.out.println("El número mrnot es: " + (int)num1);
+		}
+	}
+
+}
