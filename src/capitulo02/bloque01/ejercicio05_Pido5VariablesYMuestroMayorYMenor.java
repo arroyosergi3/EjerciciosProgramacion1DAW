3fa8a4030148a931ccd3607cc77957126e77dfd9
arroyosergi3 <sergioaj815@gmail.com>
2023-09-26 12:39:53 +0200

    feat(champter2):exercise 5 added

---
+++ src/capitulo02/bloque01/ejercicio05_Pido5VariablesYMuestroMayorYMenor.java
@@ -0,0 +1,52 @@
+package capitulo02.bloque01;
+
+import java.util.Scanner;
+
+public class ejercicio05_Pido5VariablesYMuestroMayorYMenor {
+
+	public static void main(String[] args) {
+
+		double num1, num2, num3, num4, num5, aux, aux1;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Introduce un número: ");
+		num1 = sc.nextDouble();
+		
+		System.out.println("Introduce otro número: ");
+		num2 = sc.nextDouble();
+		
+		System.out.println("Introduce otro número: ");
+		num3 = sc.nextDouble();
+		
+		System.out.println("Introduce otro número: ");
+		num4 = sc.nextDouble();
+		
+		System.out.println("Introduce otro número: ");
+		num5 = sc.nextDouble();
+		
+		if (num1 < num2) {
+			aux = num1;
+		}
+		else {
+			aux = num2;
+		}
+		if (num3 < num4) {
+			aux1 = num3;
+		}
+		else {
+			aux1 = num4;
+		}
+		if(aux < num5) {
+			aux = aux;
+		}
+		else {
+			aux = num5;
+		}
+		if(aux1 < aux) {
+			System.out.println("El número menor es " + (int)aux1);
+		}
+		else {
+			System.out.println("El número menor es " + (int)aux);
+		}
+	}
+}
+			
