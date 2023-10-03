f84bd6e28ea2c4d46bc298f5a986be5d55649daf
Sergio <Sergio@Sergio>
2023-10-03 17:50:08 +0200

    feat(chapter3, block3): Exercise 4 added

--- src/capitulo03/bloque03/ejercicio01_PideNumYSacarSumYMedia.java
+++ src/capitulo03/bloque03/ejercicio01_PideNumYSacarSumYMedia.java
@@ -6,27 +6,17 @@ public class ejercicio01_PideNumYSacarSumYMedia {
 	public static void main(String[] args) {
 		
 	Scanner sc = new Scanner(System.in);
-	int num = 1;
+	int num = 1, numPos = 0, numNeg = 0;
 	
 
 	for (int i = 0; num != 0; i++) {
 		System.out.println("Introduce un número");
 		num = sc.nextInt();
 		if (num == 0) break;
-		System.out.println(num + " * " + 0 + " = " +(num * 0));
-		System.out.println(num + " * " + 1 + " = " +(num * 1));
-		System.out.println(num + " * " + 2 + " = " +(num * 2));
-		System.out.println(num + " * " + 3 + " = " +(num * 3));
-		System.out.println(num + " * " + 4 + " = " +(num * 4));
-		System.out.println(num + " * " + 5 + " = " +(num * 5));
-		System.out.println(num + " * " + 6 + " = " +(num * 6));
-		System.out.println(num + " * " + 7 + " = " +(num * 7));
-		System.out.println(num + " * " + 8 + " = " +(num * 8));
-		System.out.println(num + " * " + 9 + " = " +(num * 9));
-		System.out.println(num + " * " + 10 + " = " +(num * 10));
-		
-		
-		
+		if (num < 0) numNeg++;
+		else numPos++;
 	}
+	System.out.println(numPos + " números son positivos");
+	System.out.println(numNeg + " números son negativos");
 }
 }
