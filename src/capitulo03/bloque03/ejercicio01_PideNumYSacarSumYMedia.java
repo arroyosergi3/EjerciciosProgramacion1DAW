bf15fab27f3ae5d986dd4a06e216d19deb6db336
Sergio <Sergio@Sergio>
2023-10-03 17:39:40 +0200

    feat (chapter3, block3): Exercise 2 added

--- src/capitulo03/bloque03/ejercicio01_PideNumYSacarSumYMedia.java
+++ src/capitulo03/bloque03/ejercicio01_PideNumYSacarSumYMedia.java
@@ -6,18 +6,21 @@ public class ejercicio01_PideNumYSacarSumYMedia {
 	public static void main(String[] args) {
 		
 	Scanner sc = new Scanner(System.in);
-	int num = 1, sumAll = 0, rep=0;
+	int num = 1, high = 0, low = 0;
 	
-	
+
 	for (int i = 0; num != 0; i++) {
 		System.out.println("Introduce un número");
 		num = sc.nextInt();
-		sumAll += num;
-		rep = i;
+		if (num == 0) break;
+		if (num > high ) high = num;
+		if (num < low ) low = num;
+		
+		
 	}
 	
-	System.out.println("La suma de los número es: " + sumAll);
-	System.out.println("La media de los número es: " + (sumAll/rep));
+	System.out.println("El número mayor es: " + high  );
+	System.out.println("El número menor es: " + low);
 
 }
 }
