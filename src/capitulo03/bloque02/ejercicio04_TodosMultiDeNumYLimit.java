eba9b3a668fda36ede5ec0c6fd666a206377ebd2
arroyosergi3 <sergioaj815@gmail.com>
2023-10-03 12:45:26 +0200

    feat(chapter3, block2):Exercise 4

--- src/capitulo03/bloque02/ejercicio04_TodosMultiDeNumYLimit.java
+++ src/capitulo03/bloque02/ejercicio04_TodosMultiDeNumYLimit.java
@@ -2,17 +2,15 @@ package capitulo03.bloque02;
 
 import java.util.Scanner;
 
-public class ejercicio03_TodosMultiDeNum {
+public class ejercicio04_TodosMultiDeNumYLimit {
 
 	public static void main(String[] args) {
-		int num, res = 1,limit;
+		int num, res = 1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("¿Qué numero quieres multiplicar?");
 		num = sc.nextInt();
-		System.out.println("¿Cuál quieres que sea el límite?");
-		limit = sc.nextInt();
 		
-		for(int i = 0; num * i < limit ; i++ ){
+		for(int i = 0; num * i < 100 ; i++ ){
 			res = (num * i);
 			System.out.println(num + " x " + i + " = " + res);
 		}
