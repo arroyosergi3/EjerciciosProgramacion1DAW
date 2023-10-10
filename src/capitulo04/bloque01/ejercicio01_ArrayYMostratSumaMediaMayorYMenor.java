ac2f3873468351b14591bf04f48154820e957e7e
arroyosergi3 <sergioaj815@gmail.com>
2023-10-10 13:00:30 +0200

    feat (chapter4, block1): Started exercise added

--- src/capitulo04/bloque01/ejercicio01_ArrayYMostratSumaMediaMayorYMenor.java
+++ src/capitulo04/bloque01/ejercicio01_ArrayYMostratSumaMediaMayorYMenor.java
@@ -3,8 +3,13 @@ package capitulo04.bloque01;
 public class ejercicio01_ArrayYMostratSumaMediaMayorYMenor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int numeros[] = new int[150];
+		for(int i = 0;i < numeros.length; i++) {
+		numeros[i] = (int) Math.round(Math.random() * 100);
+		System.out.print(numeros[i] + " ");
+		}
+		for (int i=0;)
 	}
 
 }
