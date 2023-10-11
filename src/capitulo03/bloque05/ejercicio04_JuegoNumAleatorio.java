1348c0020dc93622f1dc9bc2dbdc499e3b41d368
arroyosergi3 <sergioaj815@gmail.com>
2023-10-11 12:13:07 +0200

    feat(chapter4, block1): Exercise 1 added

--- src/capitulo03/bloque05/ejercicio04_JuegoNumAleatorio.java
+++ src/capitulo03/bloque05/ejercicio04_JuegoNumAleatorio.java
@@ -1,23 +1,28 @@
-package capitulo03.bloque05;
+package capitulo04.bloque01;
 
-import java.util.Scanner;
-
-public class ejercicio04_JuegoNumAleatorio {
-
+public class ejercicio01_ArrayYMostratSumaMediaMayorYMenor {
 	public static void main(String[] args) {
-		int numDado = 0, cont = 5;
-		int random = (int) Math.round(Math.random() * 100);
-		Scanner sc = new Scanner(System.in);
-		while (numDado != random && cont > 0 ){
-			System.out.println("Introduce un número del 0 al 100");
-			numDado = sc.nextInt();
-			if(numDado < random) System.out.println("El número es mayor");
-			if(numDado > random) System.out.println("El número es menor");
-			if (numDado == random) System.out.println("Enhorabuena, has acertado");
-			cont--;
-			if (cont == 0) System.out.println("Lo siento, has perdido");
+		float sum = 0;
+		int mayor = 0, menor = 0;
+		int numeros[] = new int[150];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int) Math.round(Math.random() * 100);
+			System.out.print(numeros[i] + " ");
 		}
-		
+		for (int i = 0; i < numeros.length; i++) {
+			sum += numeros[i];
+		}
+		System.out.println("\nLa suma de los valores del array es " + sum);
+		System.out.println("La media de los valores del array es " + (sum / (float) numeros.length));
+		menor = numeros[0];
+		mayor = numeros[0];
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] < menor)
+				menor = numeros[i];
+			if (numeros[i] > mayor)
+				mayor = numeros[i];
+		}
+		System.out.println("El número mayor es " + mayor);
+		System.out.println("El número menor es " + menor);
 	}
-
 }
--- src/capitulo04/bloque01/ejercicio01_ArrayYMostratSumaMediaMayorYMenor.java
+++ src/capitulo04/bloque01/ejercicio01_ArrayYMostratSumaMediaMayorYMenor.java
@@ -1,15 +1,28 @@
 package capitulo04.bloque01;
 
 public class ejercicio01_ArrayYMostratSumaMediaMayorYMenor {
-
 	public static void main(String[] args) {
-		
+		float sum = 0;
+		int mayor = 0, menor = 0;
 		int numeros[] = new int[150];
-		for(int i = 0;i < numeros.length; i++) {
-		numeros[i] = (int) Math.round(Math.random() * 100);
-		System.out.print(numeros[i] + " ");
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int) Math.round(Math.random() * 100);
+			System.out.print(numeros[i] + " ");
 		}
-		for (int i=0;)
+		for (int i = 0; i < numeros.length; i++) {
+			sum += numeros[i];
+		}
+		System.out.println("\nLa suma de los valores del array es " + sum);
+		System.out.println("La media de los valores del array es " + (sum / (float) numeros.length));
+		menor = numeros[0];
+		mayor = numeros[0];
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] < menor)
+				menor = numeros[i];
+			if (numeros[i] > mayor)
+				mayor = numeros[i];
+		}
+		System.out.println("El número mayor es " + mayor);
+		System.out.println("El número menor es " + menor);
 	}
-
 }
