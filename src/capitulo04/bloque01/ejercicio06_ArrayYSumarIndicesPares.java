07532dd4182e4779d574f7862e608c7fee06b00a
Sergio <Sergio@Sergio>
2023-10-12 11:11:55 +0200

    feat(chapter4, block1): Exercise 6 added

--- src/capitulo04/bloque01/ejercicio06_ArrayYSumarIndicesPares.java
+++ src/capitulo04/bloque01/ejercicio06_ArrayYSumarIndicesPares.java
@@ -2,21 +2,20 @@ package capitulo04.bloque01;
 
 import java.util.Scanner;
 
-public class ejercicio05_ArrayYSumarParesEImpares {
+public class ejercicio06_ArrayYSumarIndicesPares {
 	public static void main(String[] args) {
 
 		int numeros[] = new int[150];
-		int pairAdd = 0, oddAdd = 0;
+		int pairAdd = 0;
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i] = (int) Math.round(Math.random() * 100);
 			System.out.print(numeros[i] + " ");
-			if (numeros[i] % 2 == 0) pairAdd += numeros[i];
-			else oddAdd += numeros [i];
+			if (i % 2 == 0) pairAdd += numeros[i];
+			
 		}
-		System.out.println("\nLa suma de los números pares es " + pairAdd);
-		System.out.println("La suma de los números impares es " + oddAdd);
+		System.out.println("\nLa suma de los números con índices pares es " + pairAdd);
 
 		
 }
