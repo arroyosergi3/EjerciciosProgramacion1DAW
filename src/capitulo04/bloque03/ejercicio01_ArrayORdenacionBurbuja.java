7dd4a08483a199e3bbb45cfc751a1d19da74167e
arroyosergi3 <sergioaj815@gmail.com>
2023-10-20 11:11:11 +0200

    feat(chapter4, block3): Exercise 1 added

--- src/capitulo04/bloque03/ejercicio01_ArrayORdenacionBurbuja.java
+++ src/capitulo04/bloque03/ejercicio01_ArrayORdenacionBurbuja.java
@@ -1,10 +1,40 @@
 package capitulo04.bloque03;
 
+import utils.utilsArrays;
+
 public class ejercicio01_ArrayORdenacionBurbuja {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int array [] = new int [150];
+		utilsArrays.inicializaArray(array, 0, 100);
+		utilsArrays.imprimeArray(array);
+		ordenacionBurbuja(array);
+		System.out.println();
+		utilsArrays.imprimeArray(array);
+		
+	}
+	
+	
+	public static void ordenacionBurbuja (int a[]) {
+		
+		boolean hayCambios = true;
+		int aux = 0;
+		do {
+			hayCambios = false;
+		for(int i = 0; i < (a.length - 1);i++){
+			
+			if (a[i] > a[i+1]) {
+				aux =  a[i];
+				a[i] = a[i+1];
+				a[i +1] = aux;
+				hayCambios = true;
+			}
+			
+		}
+		
+		}
+		while (hayCambios == true);
+		
 	}
 
 }
