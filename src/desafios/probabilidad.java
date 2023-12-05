bce95e548bd00ddfb489b6b09422a9601b5dfeb8
Sergio <Sergio@Sergio>
2023-12-05 10:08:13 +0100

    desadios added

---
+++ src/desafios/probabilidad.java
@@ -0,0 +1,22 @@
+package desafios;
+
+public class probabilidad {
+
+	public static void main(String[] args) {
+		
+		//Tiene un 70% de probabilidad de acertar
+		
+		boolean haAcertado;
+		int n = utils.utils.obtenerNumeroAzar(0, 100);
+		
+		if(n<70) {
+			haAcertado = true;
+		}
+		else haAcertado = false;
+		
+		
+		
+		
+	}
+
+}
